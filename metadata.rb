d7411862d705376d5eaa94305e08df6197570159
Choe, Cheng-Dae <whitekid@gmail.com>
2014-03-12 04:09:11 +0900

    admin token 설정

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'keystone'
+maintainer       'Choe, Cheng-Dae'
+maintainer_email 'whitekid@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures openstack'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.0'
+
+supports 'ubuntu', '= 12.04'
+
+depends "openstack"
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,8 +1,30 @@
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+::Chef::Recipe.send(:include, Spadeworks::Openstack)
+
 package "keystone"
 
-ervice 'keystone' do
+mysql_node = node_by_role "mysql"
+connection = "mysql://keystone:#{mysql_node[:openstack][:mysql][:keystone_password]}@#{mysql_node[:fqdn]}/keystone"
+node.set_unless[:keystone][:admin_token] = secure_password
+
+template '/etc/keystone/keystone.conf' do
+    source 'keystone.conf.erb'
+    mode 0644
+    variables({
+        :connection => connection,
+    })
+    notifies :restart, "service[keystone]", :immediately
+end
+
+service 'keystone' do
     provider Chef::Provider::Service::Upstart
     supports :status => :true, :restart => :true, :reload => :true
     action [:enable, :start]
 end
+
+package "python-mysqldb"
+execute "keystone dbsync" do
+    command "keystone-manage db_sync"
+end
+
 
