d7c30c81f45ce7c40900330f3dbe4dba4207f7db
Choe, Cheng-Dae <whitekid@gmail.com>
2014-03-12 02:43:00 +0900

    기본 패키지만 설치

---
+++ recipes/server.rb
@@ -0,0 +1,8 @@
+package "keystone"
+
+ervice 'keystone' do
+    provider Chef::Provider::Service::Upstart
+    supports :status => :true, :restart => :true, :reload => :true
+    action [:enable, :start]
+end
+
